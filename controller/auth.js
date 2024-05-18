463624c953f42bab8e3074d3845ac7a49bd08353
alazaradane <alazargithub369@gmail.com>
2024-05-18 14:26:50 +0300

    "Updated login and logout functions in auth.js controller"

--- controller/auth.js
+++ controller/auth.js
@@ -29,32 +29,34 @@ export const register = (req,res)=>{
 
 }
 
-export const login = (req,res)=>{
-    const {username, password: userPassword} = req.body
+export const login = (req, res) => {
+    const { username, password: userPassword } = req.body;
     const q = "SELECT * FROM users WHERE username=?";
-
-    db.query(q,[username],(err,data)=>{
-        if(err) return res.json(err);
-        if(data.length == 0) return res.status(404).json('User not found!!!')
-        
-        // Checking password
-        const isPasswordCorrect = bcrypt.compareSync(userPassword, data[0].password);
-        if(!isPasswordCorrect) return res.status(400).json("username or password not correct");
-
-        const token = jwt.sign({id: data[0].id}, 'jwtkey',{ expiresIn: '1h' });
-        const {password, ...other} = data[0]
-
-        res.cookie("access_token",token,{
-            httpOnly:true,
-        }).status(200).json(other)
-
-    })
-}
-
-export const logout = (req,res)=>{
-    
-    res.clearCookie("access_token",{
-        sameSite:"none",
-        secure:true
-    }).status(200).json("User has been logged out.")
-}
+  
+    db.query(q, [username], (err, data) => {
+      if (err) return res.json(err);
+      if (data.length == 0) return res.status(404).json('User not found!!!');
+      
+      // Checking password
+      const isPasswordCorrect = bcrypt.compareSync(userPassword, data[0].password);
+      if (!isPasswordCorrect) return res.status(400).json("username or password not correct");
+  
+      // Generate new token
+      const token = jwt.sign({ id: data[0].id }, 'jwtkey', { expiresIn: '1m' }); // Token expires in 1 hour
+      const { password, ...other } = data[0];
+  
+      res.cookie("access_token", token, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,  // use secure:true for HTTPS
+      }).status(200).json(other);
+    });
+  };
+  
+  export const logout = (req, res) => {
+    res.clearCookie("access_token", {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,  
+    }).status(200).json("User has been logged out.");
+  };
