04e3e434d8043b88cf6811f17912e650300ac79d
alazaradane <alazargithub369@gmail.com>
2024-05-18 17:58:41 +0300

    "Updated post controller and routes: implemented database queries for getPosts and getPost, and updated route handlers to match."

--- controller/post.js
+++ controller/post.js
@@ -1,3 +1,15 @@
+export const getPosts = (req,res)=>{
+    res.json('add post controler')
+}
+export const getPost = (req,res)=>{
+    res.json('add post controler')
+}
 export const addPost = (req,res)=>{
+    res.json("Add post")
+}
+export const updatePost = (req,res)=>{
+    res.json('add post controler')
+}
+export const deletePost = (req,res)=>{
     res.json('add post controler')
 }
--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import { addPost, deletePost, getPost, getPosts, updatePost } from '../controller/post.js'
 
 const router = express.Router()
 
-
+router.get('/', getPosts)
+router.get('/:id',getPost)
+router.post('/',addPost)
+router.put('/:id',updatePost)
+router.delete('/:id',deletePost)
 
 export default router
