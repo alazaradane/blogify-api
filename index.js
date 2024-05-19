cbec3e3dc0b7b96a306f1910250fa515acb27848
alazaradane <alazargithub369@gmail.com>
2024-05-19 11:39:34 +0300

    "Added multer dependency and implemented file upload functionality"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import multer from 'multer'
 import postRoutes from './routes/post.js'
 import userRoutes from './routes/user.js'
 import authRoutes from './routes/auth.js'
@@ -12,6 +13,22 @@ app.use(cors({
     origin: 'http://localhost:5173', // frontend URL
     credentials: true, // Allow credentials (cookies)
 }))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../blogify-client/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname)
+    }
+  })
+  
+const upload = multer({ storage })
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    const file = req.file
+    res.status(200).json(file.filename)
+  })
+
 app.use('/api/posts', postRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
